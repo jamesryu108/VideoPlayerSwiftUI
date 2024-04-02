a09ac0f0d6a3a752d67f5afe2809312fed0fb206
therealjamesryu <33236626+jamesryu108@users.noreply.github.com>
2024-04-02 14:34:17 -0400

    Implement `AVPlayer` integration with SwiftUI using `UIViewRepresentable`
    - Created `AVPlayerView` as a `UIViewRepresentable` to bridge `AVPlayer` with SwiftUI, enabling video playback within SwiftUI views.
    - Defined `PlayerUIView`, a UIView subclass, to host `AVPlayerLayer`, ensuring proper video display within the SwiftUI layout.
    - In `PlayerUIView` initialization, configured and added `AVPlayerLayer` to the view's layer, setting the videoGravity to `.resizeAspect` for appropriate aspect ratio handling.
    - Implemented layoutSubviews in `PlayerUIView` to dynamically adjust the `AVPlayerLayer`'s frame to match the `UIView` bounds, ensuring the video content scales correctly with view size changes.
    - Provided a clean and reusable solution for integrating complex UIKit components, such as `AVPlayer`, into SwiftUI applications, enhancing the app's multimedia capabilities.

--- VideoPlayerSwiftUI/VideoPlayer/AVPlayerView.swift
+++ VideoPlayerSwiftUI/VideoPlayer/AVPlayerView.swift
@@ -14,14 +14,31 @@ struct AVPlayerView: UIViewRepresentable {
 	var player: AVPlayer
 
 	func makeUIView(context: Context) -> UIView {
-		let view = UIView()
-		let layer = AVPlayerLayer(player: player)
-		layer.frame = view.bounds
-		layer.videoGravity = .resizeAspect
-		view.layer.addSublayer(layer)
-		return view
+		return PlayerUIView(player: player)
 	}
 
-	func updateUIView(_ uiView: UIView, context: Context) {
+	func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
+class PlayerUIView: UIView {
+	private var playerLayer: AVPlayerLayer?
+
+	init(player: AVPlayer) {
+		super.init(frame: .zero)
+		// Initialize the player layer and add it to the view's layer
+		let playerLayer = AVPlayerLayer(player: player)
+		self.layer.addSublayer(playerLayer)
+		self.playerLayer = playerLayer
+		playerLayer.videoGravity = .resizeAspect
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		// Ensure the player layer bounds match the UIView bounds
+		playerLayer?.frame = self.bounds
 	}
 }
