956d64673edb3c0644f3125065ba8096685cd8a0
therealjamesryu <33236626+jamesryu108@users.noreply.github.com>
2024-04-02 04:29:42 -0400

    Add `AttributedTextView` SwiftUI wrapper for `NSAttributedString` display and add it to `ContentView`
    - Create`AttributedTextView` struct to wrap `UITextView`, enabling `NSAttributedString` display within SwiftUI views.
    - Configure `UITextView` to be read-only and scrollable for enhanced user interaction.
    - Implement `UIViewRepresentable` protocol methods to integrate UIKit `UITextView` with SwiftUI.
    
    - Implement `LazyVStack` in ContentView for improved performance with dynamic content.
    - Move navigation bar customization logic into a dedicated method, `customizeNavigationBar`, for better code organization and readability.
    - Adjust `ContentView` to use `GeometryReader` for responsive layout design, ensuring compatibility with various device sizes and orientations.

--- VideoPlayerSwiftUI/ContentView.swift
+++ VideoPlayerSwiftUI/ContentView.swift
@@ -5,11 +5,37 @@
 //  Created by Michael Gauthier on 2021-01-06.
 //
 
+import MarkdownKit
 import SwiftUI
 
 struct ContentView: View {
-	
+
+	private let markdownString = "This is **bold**, this is *italic*, and this is `code`."
+	private let markdownParser = MarkdownParser()
+
 	init() {
+		customizeNavigationBar()
+	}
+
+	var body: some View {
+		NavigationView {
+			GeometryReader { geometry in
+				LazyVStack(spacing: 0) {
+					VideoPlayerControlsView()
+						.frame(height: geometry.size.height * 0.3) // 30% of the parent view's height
+					ScrollView {
+						AttributedTextView(attributedString: markdownParser.parse(markdownString))
+							.frame(height: geometry.size.height * 0.7)
+							.border(Color.gray, width: 1) // Optional: Add a border for visibility
+					}
+				}
+				.navigationBarTitle("Video Player", displayMode: .inline)
+			}
+		}
+	}
+
+	private func customizeNavigationBar() {
+		// Customization logic moved here for clarity
 		// Customize navigation bar appearance
 		let appearance = UINavigationBarAppearance()
 		appearance.configureWithOpaqueBackground()
@@ -22,19 +48,6 @@ struct ContentView: View {
 		UINavigationBar.appearance().compactAppearance = appearance // For smaller navigation bars
 		UINavigationBar.appearance().scrollEdgeAppearance = appearance // For large titles
 		UINavigationBar.appearance().tintColor = .white // Set the tint color for buttons
-	}
-
-	var body: some View {
-		NavigationView {
-			GeometryReader { geometry in
-				VStack {
-					VideoPlayerControlsView()
-						.frame(height: geometry.size.height * 0.3) // 30% of the parent view's height
-					Spacer() // Use a Spacer to push the video player to the top
-				}
-				.navigationBarTitle("Video Player", displayMode: .inline)
-			}
-		}
 	}
 }
 
